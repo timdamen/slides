8bef2f5f0b86cdc4ee1aacc87fe1aa0e340f8c93
timdamen <hello@timdamen.io>
2026-06-23 07:59:36 +0200

    feat: devdays presentation

---
+++ presentations/devdays-26/global-bottom.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<style>
+h3 {
+  text-transform: none !important;
+  color: #ffffff !important;
+  font-size: 1.35rem !important;
+  font-weight: 600 !important;
+  opacity: 1 !important;
+}
+</style>
